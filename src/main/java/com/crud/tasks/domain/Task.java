d37ac617f31b2676349694dfc4a36fba6946cd9b
lukstasko <119363165+lukstasko@users.noreply.github.com>
2023-04-25 23:10:12 +0200

    24.1 Added index.html

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,13 +1,14 @@
 package com.crud.tasks.domain;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Getter
 @AllArgsConstructor
