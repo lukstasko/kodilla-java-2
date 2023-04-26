177e89595086e87988e84420f9fb2f3eba711fa9
lukstasko <119363165+lukstasko@users.noreply.github.com>
2023-04-26 22:39:49 +0200

    24.2 Update created-trello-card-mail

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,8 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,9 @@ import java.util.List;
 
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    private DBService dbService;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -33,18 +38,41 @@ public class MailCreatorService {
         functionality.add("Application allows sending tasks to Trello");
 
         Context context = new Context();
-        context.setVariable("message", message);
         context.setVariable("tasks_url","https://lukstasko.github.io/");
-        context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("goodbye_message", "Have a nice day!");
-        context.setVariable("company_details", companyDetails);
-        context.setVariable("show_button", false);
-        context.setVariable("is_friend", true);
-        context.setVariable("admin_config", adminConfig);
+        context.setVariable("button","Visit website");
+        context.setVariable("admin_name",adminConfig.getAdminName());
+        context.setVariable("goodbye_message","Have a nice day!");
+        context.setVariable("company_details",companyDetails);
+        context.setVariable("is_friend",true);
+        context.setVariable("admin_config",adminConfig);
+        context.setVariable("message", message);
+        context.setVariable("show_button",false);
+
         context.setVariable("application_functionality", functionality);
 
         return templateEngine.process("mail/created-trello-card-mail", context);
 
     }
+
+    public String buildTaskDailyReportMail(){
+
+        List<Task> taskList=dbService.getAllTasks();
+        String message="You have " + taskList.size()+" tasks today:" ;
+
+        Context raportContext = new Context();
+        raportContext.setVariable("message", message);
+        raportContext.setVariable("tasks_url","https://lukstasko.github.io/");
+        raportContext.setVariable("button", "Visit website");
+        raportContext.setVariable("goodbye_message", "Have a nice day!");
+        raportContext.setVariable("company_details", companyDetails);
+        raportContext.setVariable("show_button", true);
+        raportContext.setVariable("is_friend", true);
+        raportContext.setVariable("admin_config", adminConfig);
+        raportContext.setVariable("application_functionality", taskList);
+
+        return templateEngine.process("mail/task_daily_report_mail", raportContext);
+
+
+    }
+
 }
